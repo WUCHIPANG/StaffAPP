4d68d80279d9d415718669230dd1cb005e1c1a81
olga.wu <chipang59@gmail.com>
2021-05-09 23:13:41 +0800

    0507

--- src/main.js
+++ src/main.js
@@ -9,14 +9,52 @@ import CoreuiVue from '@coreui/vue'
 import {iconsSet as icons} from './assets/icons/icons.js'
 import store from '@/sotre'
 window.$ = window.jQuery = require('jquery')
-import firebaseMessaging from 'firebase'
 
-Vue.prototype.$messaging = firebaseMessaging
+import firebase from 'firebase'
+// import '../public/firebase-messaging-sw'
+Vue.config.productionTip = false
 Vue.config.performance = true
 Vue.use(Vuex)
 Vue.use(VueAxios, axios)
 Vue.use(CoreuiVue)
 Vue.prototype.$log = console.log.bind(console)
+
+const config = {
+  apiKey: 'AIzaSyCSkb2PtrcULZdnFxSoJNLImCWiAmODftw',
+  authDomain: 'dyna-cloud.firebaseapp.com',
+  projectId: 'dyna-cloud',
+  storageBucket: 'dyna-cloud.appspot.com',
+  messagingSenderId: '752582535911',
+  appId: '1:752582535911:web:a1a064b13ae5cda3d69042',
+  measurementId: 'G-HY99D25VGV',
+}
+
+// firebase.initializeApp(config)
+
+Vue.prototype.$messaging = null
+if (firebase.messaging.isSupported()) {
+  firebase.initializeApp(config)
+  // Retrieve Firebase Messaging object, assign to Vue Object
+  Vue.prototype.$messaging = firebase.messaging()
+  // Add the public key generated from the Firebase console
+  Vue.prototype.$messaging.usePublicVapidKey(
+    'BFAWhU1AnnjtL2WHMzsay0zhi7XLLFMoOhxpY4_tmtOt7Ze_KwVwasd2CAa6S5FKUYaCKtif58Ft4-eiBAAUEok'
+  )
+}
+console.log('main.js/usePublicVapidKey')
+//   BFAWhU1AnnjtL2WHMzsay0zhi7XLLFMoOhxpY4_tmtOt7Ze_KwVwasd2CAa6S5FKUYaCKtif58Ft4-eiBAAUEok
+
+// Change server-worker.js register path
+navigator.serviceWorker
+  .register('/public/firebase-messaging')
+  .then((registration) => {
+    console.log('main.js/public/firebase-messaging')
+    Vue.prototype.$swRegistration = registration
+    Vue.prototype.$messaging.useServiceWorker(registration)
+  })
+  .catch((err) => {
+    console.log(err)
+  })
 
 new Vue({
   el: '#app',
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -13,23 +13,7 @@ console.log('hello')
                 <CCardBody>
                   <CForm>
                     <h1>傳送人員登入</h1>
-                    <!-- <input type="hidden" id="device_id" value="" />
-
-                    <div class="col-md-5 unbind" style="">
-                      <label for="username" class="form-label">綁定名稱：</label><input type="text" id="username" class="form-control" placeholder="請輸入名稱" />
-                    </div>
-                    <input id="device_id" type="hidden" />
-                    <div class="col-md-5 unbind" style="">
-                      <button type="button" id="bind" class="btn btn-warning" @click="testbtn()">
-                        綁定
-                      </button>
-                    </div>
-                    <div class="col-md-5 bind" style="display: none;">
-                      <label for="username" class="form-label"
-                        >已綁定名稱：
-                        <p id="bind_name"></p>
-                      </label>
-                    </div> -->
+                  
                     <div class="col-md-5 bind" style="display: none;"><button type="button" id="unbind" class="btn btn-warning" @click="unbind()">解除綁定</button></div>
 
                     <CInput placeholder="帳號" autocomplete="username" v-model="username">
@@ -46,11 +30,9 @@ console.log('hello')
                           id="bind"
                           size="lg"
                           class="px-4"
-                          @click="
-                            login(username, password);                        
-                          "
-                          >登入</CButton
+                          @click="login(username, password); ">登入</CButton
                         >
+                        <button @click="registeFCM">Register</button>
                       </CCol>
                       <CCol col="6" class="text-right">
                         <CButton color="link" class="px-0" @click="darkModal = true" size="lg">忘記密碼?</CButton>
@@ -84,6 +66,7 @@ console.log('hello')
 
 
 <script> 
+import 'firebase/messaging'
 import Global from '@/Global';
 const CryptoJS = require('crypto-js');
 import secret from '../utils/secret';
@@ -96,144 +79,63 @@ export default {
       username: '',
       password: '',
       darkModal: false,
-       inject: ['messaging'],
     };
   },
-//   setup() {
-//     const messaging = inject('messaging')
-
-//     console.log('Firebase cloud messaging object', messaging)
-//   },
-//   mounted() {
-
-//       console.log('Firebase cloud messaging object', this.$messaging)
-//       messaging.getToken({ vapidKey: 'BFAWhU1AnnjtL2WHMzsay0zhi7XLLFMoOhxpY4_tmtOt7Ze_KwVwasd2CAa6S5FKUYaCKtif58Ft4-eiBAAUEok' })
-    
-//     app.messaging().getToken({ vapidKey: 'BFAWhU1AnnjtL2WHMzsay0zhi7XLLFMoOhxpY4_tmtOt7Ze_KwVwasd2CAa6S5FKUYaCKtif58Ft4-eiBAAUEok' })
-// .then((currentToken) => {
-//   if (currentToken) {
-//     console.log('client token', currentToken)
-//   } else {
-//     console.log('No registration token available. Request permission to generate one.');
-//   }
-// }).catch((err) => {
-//   console.log('An error occurred while retrieving token. ', err);
-// })
-    //  
-    // console.log('123');
-    // $('.bind').hide();
-    // $('.unbind').hide();
-    // setTimeout(function() {
-    // //   var device_id = $('#device_id').val();
-    //   console.log('device_id', device_id);
-    //   $.post(
-    //     'https://porter-alpha.dynacloud.co/ajax.php',
-    //     {
-    //       device_id: device_id,
-    //       action: 'check',
-    //     },
-    //     function(result) {
-    //       if (result.status == true) {
-    //         $('#bind_name').html(result.message);
-    //         $('#send_user')
-    //           .find('option[value="' + result.id + '"]')
-    //           .attr('selected', true);
-    //         $('.bind').show();
-    //       } else {
-    //         $('.unbind').show();
-    //       }
-    //     },
-    //     'json'
-    //   );
-    // }, 1000);
-//   },
-  methods: {
-    login() {
-let data = {
+mounted () {
+    console.log('hello')
+    this.initFCM()
+  },
+    methods: {
+           login() {
+        let data = {
           user: this.username,
           password: secret.Encrypt(this.password),
         };
 
         this.$store.dispatch('userLogin', data);
     },
-    testbtn(event) {
-      console.log('測試');
-      $('#bind').click(function(event) {
-        console.log('綁定');
-        if (event.preventDefault) {
-          event.preventDefault();
-        } else {
-          event.returnValue = false;
-        }
-        var username = $('#username').val();
-        var device_id = $('#device_id').val();
-        var msg = '';
-        if (username == '') {
-          msg = msg + '請輸入名稱\n';
-        }
-        if (device_id == '') {
-          msg = msg + '請先允許瀏覽器取得通知權限\n';
-        }
-        if (msg == '') {
-          $.post(
-            'https://porter-alpha.dynacloud.co/ajax.php',
-            {
-              username: username,
-              device_id: device_id,
-              action: 'bind',
-            },
-            function(result) {
-              if (result.status == true) {
-                window.location.reload();
-              } else {
-                alert(result.message);
-              }
-            },
-            'json'
-          );
-        } else {
-          alert(msg);
-        }
-      });
+    initFCM () {
+        console.log('initFCM')
+      this.$messaging.onTokenRefresh(() => {
+        this.$messaging.getToken().then((refreshedToken) => {
+          console.log('Token refreshed.')
+          this.setTokenSentToServer(false)
+          this.sendTokenToServer(refreshedToken)
+        })
+      })
     },
-    unbind(event) {
-      console.log('解除');
-      //解除綁定
-      $('#unbind').click(function(event) {
-        if (event.preventDefault) {
-          event.preventDefault();
-        } else {
-          event.returnValue = false;
-        }
-        var device_id = $('#device_id').val();
-        var msg = '';
-        if (device_id == '') {
-          msg = msg + '請輸入要解綁的Token\n';
-        }
-        if (msg == '') {
-          $.post(
-            'https://porter-alpha.dynacloud.co/ajax.php',
-            {
-              device_id: device_id,
-              action: 'unbind',
-            },
-            function(result) {
-              console.log(result);
-              if (result.status == true) {
-                alert(result.message);
-                window.location.reload();
-              } else {
-                alert(result.message);
-              }
-            },
-            'json'
-          );
-        } else {
-          alert(msg);
-        }
-      });
+    registeFCM () {
+      this.$messaging.requestPermission().then(() => {
+        console.log('Notification permission granted.')
+        this.getToken()
+      }).catch((err) => {
+        console.log('Unable to get permission to notify.', err)
+      })
     },
-  },
+    getToken () {
+      this.$messaging.getToken().then((currentToken) => {
+        if (currentToken) {
+          this.sendTokenToServer(currentToken)
+        } else {
+          console.log('No Instance ID token available. Request permission to generate one.')
+          // Show permission UI.
+          this.setTokenSentToServer(false)
+        }
+      }).catch((err) => {
+        console.log('An error occurred while retrieving token. ', err)
+        this.setTokenSentToServer(false)
+      })
+    },
+    sendTokenToServer (token) {
+      // TODO: Send Token To Your Server
+    },
+    setTokenSentToServer (type) {
+      if (type) return
+      // TODO: Delete Register Token From Your Server
+    }
+
+  }
+
 };
 </script>
 <style>
